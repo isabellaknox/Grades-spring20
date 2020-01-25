046ac64308ff9f3d39875636a8de5635b4714b3b
Isabella Knox <ik63@duke.edu>
2020-01-25 21:41:44 +0000

    4a ch

--- bySeventeen.c
+++ bySeventeen.c
@@ -4,14 +4,18 @@
 int main(){
     int N;
     int x;
+    int tot;
 
     //get given integer
-    printf("Enter integer: ");
-    scanf("%s", N);
+    printf("Enter integer to get answer: ");
+    scanf("%d", &N);
 
     // get first N numbers divisible by 17 
-    for (x =0; x <= N; x++){
-        printf("%d", 17*x)
+    if(N>0){
+    for (x =1; x <= N; x++){
+        tot = 17 * x;
+        printf("%d\n", tot);
+        }
     }
 
     // successfully exit program
